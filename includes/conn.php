18781b1a4c7a5ec71f815b99cc966a864b21340a
mohamedtraoreledev <mohamedtraora981@gmail.com>
2026-05-22 17:57:48 +0000

    version nouveau

--- includes/conn.php
+++ includes/conn.php
@@ -27,7 +27,7 @@
 
 
     try{
-        $bd = new PDO("mysql:host=127.0.0.1;port=3308;dbname=ballo;charset=utf8","root","TRA123");
+        $bd = new PDO("mysql:host=mysql://root:PgnEHOHbhDvJybGCBHgCnYfgNGDGKDII@kodama.proxy.rlwy.net:55129/railway;port=55129;dbname=railway;charset=utf8","root","PgnEHOHbhDvJybGCBHgCnYfgNGDGKDII");
         $bd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         // echo "<p style=color:white>connexion reussi</p> ";
     }catch(Exception $e){
